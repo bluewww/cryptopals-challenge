6513f982bfa31a1c2d8970b68d7160b197ceeed6
bluew <bluewww@users.noreply.github.com>
2024-01-28 23:53:55 +0100

    Move reusable functions to lib

--- cryptic/set2.py
+++ cryptic/set2.py
@@ -10,7 +10,7 @@ import secrets
 from Crypto.Cipher import AES
 
 from basic import (aes_cbc_decrypt, aes_cbc_encrypt, aes_detect_ecb_block_mode,
-                   aes_ecb_encrypt)
+                   aes_ecb_encrypt, aes_ecb_pad_attack, strip_padding)
 
 
 def pad_with_pkcs7(ba, align):
@@ -91,61 +91,6 @@ def aes_ecb_oracle(text):
         global_key)
 
 
-def aes_ecb_pad_attack(oracle):
-    # Discover the block cipher size
-    blocksize = -1
-    base = len(oracle(b''))
-    for i in range(128):
-        feed = b'A' * i
-        new = len(oracle(feed))
-        if new != base:
-            blocksize = new - base
-            break
-
-    # Discover attacker controlled offset caused by random prefix. For that, we
-    # find first repeated block controlled by us. This allows us to deduce the
-    # alignment of our input
-    def find_offset(oracle):
-        for k in range(2 * blocksize, 3 * blocksize):
-            enc = oracle(k * b'A')
-            prev = enc[0:blocksize]
-            for i in range(blocksize, len(enc), blocksize):
-                block = enc[i:i+blocksize]
-                if block == prev:
-                    return 2 * blocksize - k + (i - blocksize)
-                prev = block
-        raise Exception('could not determine input alignment')
-
-    goff = find_offset(oracle)
-    align = goff % blocksize
-    # round to next blocksize
-    goff = -goff % blocksize + goff
-
-    # Check if ciphertext is really ecb encrypted
-    if not aes_detect_ecb_block_mode(oracle):
-        raise Exception('oracle does not seem to use ECB mode')
-
-    # Let one byte from the unknown data shift into our encrypted block. Then,
-    # bruteforce all 256 possibilities by matching encrypted blocks.
-    # Afterwards, use the decoded byte to attack the following byte.
-    shift = bytearray(b'A' * blocksize)
-    for off in range(0, base, blocksize):
-        for k in range(blocksize):
-            if align > 0:
-                pad = b'X' * (blocksize - align)
-            else:
-                pad = b''
-            feed = b'A' * (blocksize - 1 - k)
-            expected = oracle(pad + feed)[goff+off:goff+off+blocksize]
-            for char in range(256):
-                plaintext = (pad + shift[off+k+1:off+k+blocksize] +
-                             char.to_bytes(1, 'big'))
-                if oracle(plaintext)[goff:goff+blocksize] == expected:
-                    shift += char.to_bytes(1, 'big')
-                    break
-    return shift[blocksize:], blocksize
-
-
 print('Challenge 12')
 dec, blocksize = aes_ecb_pad_attack(aes_ecb_oracle)
 print('blocksize =', blocksize)
@@ -182,15 +127,6 @@ print('dec =', dec)
 
 # Challenge 15
 # PKCS#7 padding validation
-
-def strip_padding(text, align):
-    if len(text) % align != 0:
-        raise Exception('Invalid alignment')
-    padval = text[-1]
-    for n in range(1, padval+1):
-        if text[-n] != padval:
-            raise Exception('Invalid padding')
-    return text[:-padval]
 
 
 print('Challenge 15')
